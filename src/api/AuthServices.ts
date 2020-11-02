7ea9fd94aa6e3afcdaa7412e4a06f508f9d5126c
hungtd <hungtd@kaopiz.com>
2020-11-02 13:57:01 +0700

    save token

--- src/api/AuthServices.ts
+++ src/api/AuthServices.ts
@@ -1,5 +1,6 @@
 import {fetchData, MethodHTTP} from './ApiService'
 import {SIGN_IN} from './ApiPath'
+import {saveToken} from '../utils/DataUtil'
 
 export async function SignIn(
   loginId: string,
@@ -7,13 +8,16 @@ export async function SignIn(
 ): Promise<Boolean> {
   const promise = new Promise<Boolean>(async (resolve, reject) => {
     try {
-      await fetchData(SIGN_IN, MethodHTTP.POST, {
+      const reponse = await fetchData(SIGN_IN, MethodHTTP.POST, {
         login_id: loginId,
         password: password,
         login_type: 1,
       })
+      await saveToken(reponse.data.data.original.access_token)
       resolve(true)
     } catch (error) {
+      console.log(error)
+      console.log(error.response)
       reject(error)
     }
   })
